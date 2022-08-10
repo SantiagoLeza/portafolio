c6cfafcf4d4fa0e277e6b79aca2de7e951315284
Santiago Leza <santiago.leza@hotmail.com>
2022-08-10 17:33:10 -0300

    2.0 cv updated

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ListRepositories from './ListRepositories'
 import ProfileInfo from './ProfileInfo'
 import './Github.css'
